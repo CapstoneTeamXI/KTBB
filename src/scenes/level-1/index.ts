1b187ad9fdccfd5e32b766d3cf29eb6e95cf8b08
hujeffy1 <hujeffy.1@gmail.com>
2022-03-18 18:31:33 -0400

    Added new map for Level1

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -1,6 +1,6 @@
-import { GameObjects, Scene, Tilemaps } from "phaser";
-import { Player } from "../../classes/player";
-import { gameObjectsToObjectPoints } from "../../helpers/gameobject-to-object-point";
+import { GameObjects, Scene, Tilemaps } from 'phaser';
+import { Player } from '../../classes/player';
+import { gameObjectsToObjectPoints } from '../../helpers/gameobject-to-object-point';
 
 export class Level1 extends Scene {
   private player!: Player;
@@ -12,18 +12,18 @@ export class Level1 extends Scene {
 
   private initMap(): void {
     this.map = this.make.tilemap({
-      key: "dungeon",
+      key: 'dungeon',
       tileWidth: 16,
       tileHeight: 16,
     });
-    this.tileset = this.map.addTilesetImage("dungeon", "tiles");
+    this.tileset = this.map.addTilesetImage('dungeon', 'tiles');
     this.groundLayer = this.map.createDynamicLayer(
-      "Ground",
+      'Ground',
       this.tileset,
       0,
       0
     );
-    this.wallsLayer = this.map.createDynamicLayer("Walls", this.tileset, 0, 0);
+    this.wallsLayer = this.map.createDynamicLayer('Walls', this.tileset, 0, 0);
     this.physics.world.setBounds(
       0,
       0,
@@ -35,7 +35,7 @@ export class Level1 extends Scene {
   }
 
   constructor() {
-    super("level-1-scene");
+    super('level-1-scene');
   }
 
   private showDebugWalls(): void {
@@ -45,39 +45,37 @@ export class Level1 extends Scene {
       collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
     });
   }
-  private initChests(): void {
-    const chestPoints = gameObjectsToObjectPoints(
-      this.map.filterObjects('Chests', obj => obj.name === 'ChestPoint'),
-    );
-    this.chests = chestPoints.map(chestPoint =>
-      this.physics.add.sprite(chestPoint.x, chestPoint.y, 'tiles_spr', 595).setScale(1.5),
-    );
-    this.chests.forEach(chest => {
-      this.physics.add.overlap(this.player, chest, (obj1, obj2) => {
-        obj2.destroy();
-        this.cameras.main.flash();
-      });
-    });
-    const monsterChest = gameObjectsToObjectPoints(
-      this.map.filterObjects('Chests', obj => obj.name === 'MonsterChest'),
-    );
-    this.chests = monsterChest.map(monsterChest =>
-      this.physics.add.sprite(monsterChest.x, monsterChest.y, 'tiles_spr', 661).setScale(1.5),
-    );
-    this.chests.forEach(chest => {
-      this.physics.add.overlap(this.player, chest, (obj1, obj2) => {
-        obj2.destroy();
-        this.cameras.main.flash();
-      });
-    });
-  }
-
-
+  // private initChests(): void {
+  //   const chestPoints = gameObjectsToObjectPoints(
+  //     this.map.filterObjects('Chests', obj => obj.name === 'ChestPoint'),
+  //   );
+  //   this.chests = chestPoints.map(chestPoint =>
+  //     this.physics.add.sprite(chestPoint.x, chestPoint.y, 'tiles_spr', 595).setScale(1.5),
+  //   );
+  //   this.chests.forEach(chest => {
+  //     this.physics.add.overlap(this.player, chest, (obj1, obj2) => {
+  //       obj2.destroy();
+  //       this.cameras.main.flash();
+  //     });
+  //   });
+  //   const monsterChest = gameObjectsToObjectPoints(
+  //     this.map.filterObjects('Chests', obj => obj.name === 'MonsterChest'),
+  //   );
+  //   this.chests = monsterChest.map(monsterChest =>
+  //     this.physics.add.sprite(monsterChest.x, monsterChest.y, 'tiles_spr', 661).setScale(1.5),
+  //   );
+  //   this.chests.forEach(chest => {
+  //     this.physics.add.overlap(this.player, chest, (obj1, obj2) => {
+  //       obj2.destroy();
+  //       this.cameras.main.flash();
+  //     });
+  //   });
+  // }
 
   create(): void {
     this.initMap();
     this.player = new Player(this, 100, 100);
-    this.initChests();
+    // this.initChests();
     this.physics.add.collider(this.player, this.wallsLayer);
   }
 
