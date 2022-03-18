c4dee0fe944123123aad97e4b2daf41eb04cede2
Jake Sucoff <jsucoff@gmail.com>
2022-03-18 12:49:53 -0400

    added map and started collision logic

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -1,14 +1,51 @@
-import { GameObjects, Scene } from 'phaser';
-import { Player } from '../../classes/player';
+import { GameObjects, Scene, Tilemaps } from "phaser";
+import { Player } from "../../classes/player";
 
 export class Level1 extends Scene {
-  // private king!: GameObjects.Sprite;
   private player!: Player;
-  constructor() {
-    super('level-1-scene');
+  private map!: Tilemaps.Tilemap;
+  private tileset!: Tilemaps.Tileset;
+  private wallsLayer!: Tilemaps.DynamicTilemapLayer;
+  private groundLayer!: Tilemaps.DynamicTilemapLayer;
+
+  private initMap(): void {
+    this.map = this.make.tilemap({
+      key: "dungeon",
+      tileWidth: 16,
+      tileHeight: 16,
+    });
+    this.tileset = this.map.addTilesetImage("dungeon", "tiles");
+    this.groundLayer = this.map.createDynamicLayer(
+      "Ground",
+      this.tileset,
+      0,
+      0
+    );
+    this.wallsLayer = this.map.createDynamicLayer("Walls", this.tileset, 0, 0);
+    this.physics.world.setBounds(
+      0,
+      0,
+      this.wallsLayer.width,
+      this.wallsLayer.height
+    );
+    this.wallsLayer.setCollisionByProperty({ collides: true });
+    this.showDebugWalls();
   }
+
+  constructor() {
+    super("level-1-scene");
+  }
+
+  private showDebugWalls(): void {
+    const debugGraphics = this.add.graphics().setAlpha(0.7);
+    this.wallsLayer.renderDebug(debugGraphics, {
+      tileColor: null,
+      collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
+    });
+  }
+
   create(): void {
-    // this.king = this.add.sprite(100, 100, 'king');
+    this.initMap();
     this.player = new Player(this, 100, 100);
   }
 
--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -1,23 +1,29 @@
-import { GameObjects, Scene } from 'phaser';
+import { GameObjects, Scene } from "phaser";
 export class LoadingScene extends Scene {
   constructor() {
-    super('loading-scene');
+    super("loading-scene");
   }
 
   preload(): void {
-    this.load.baseURL = 'assets/';
+    this.load.baseURL = "assets/";
 
-    this.load.image('king', 'sprites/king.png');
+    this.load.image("king", "sprites/king.png");
 
-    // Our king atlas
     this.load.atlas(
-      'a-king',
-      'spritesheets/a-king.png',
-      'spritesheets/a-king_atlas.json'
+      "a-king",
+      "spritesheets/a-king.png",
+      "spritesheets/a-king_atlas.json"
     );
+
+    this.load.image({
+      key: "tiles",
+      url: "tilemaps/tiles/dungeon-16-16.png",
+    });
+
+    this.load.tilemapTiledJSON("dungeon", "tilemaps/json/dungeon.json");
   }
 
   create(): void {
-    this.scene.start('level-1-scene');
+    this.scene.start("level-1-scene");
   }
 }
