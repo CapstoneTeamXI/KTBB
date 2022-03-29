0aeeccfc5ed30eda6b1d333c1aa7f87420479565
hujeffy1 <hujeffy.1@gmail.com>
2022-03-29 16:34:39 -0400

    on click sends to leaderboard submission

--- src/scenes/ui/index.ts
+++ src/scenes/ui/index.ts
@@ -20,7 +20,6 @@ export class UIScene extends Scene {
   private interval: NodeJS.Timer;
   private currentScene!: string;
   private prevScene!: string;
-  private bossKillHandler: (status: GameStatus) => void;
 
   constructor() {
     super("ui-scene");
@@ -43,24 +42,16 @@ export class UIScene extends Scene {
     this.gameEndHandler = (status) => {
       if (status === GameStatus.WIN) {
         this.score.changeValue(ScoreOperations.INCREASE, 2000);
-        store.dispatch<any>({ type: GAME_OVER });
-        store.dispatch<any>({
-          type: GET_GAME_STATS,
-          gameStats: {
-            score: this.score.getValue(),
-            completedTime: this.timer.getValue(),
-          },
-        });
       }
       this.cameras.main.setBackgroundColor("rgba(0,0,0,0.6)");
-      this.game.scene.pause(this.currentScene);
+      this.game.scene.pause(this.currentScene.replaceAll('"', ""));
       this.gameEndPhrase = new Text(
         this,
         this.game.scale.width / 2,
         this.game.scale.height * 0.4,
         status === GameStatus.LOSE
           ? `YOU'VE BEEN SLAIN!\nCLICK TO RESTART`
-          : `YOU ARE VICTORIOUS!\nCLICK TO RESTART`
+          : `YOU ARE VICTORIOUS!\nCLICK TO POST YOUR\nSCORE ON THE LEADERBOARD`
       )
         .setAlign("center")
         .setColor(status === GameStatus.LOSE ? "#ff0000" : "#ffffff");
@@ -70,6 +61,16 @@ export class UIScene extends Scene {
       );
       this.alive = false;
       this.input.on("pointerdown", () => {
+        if (status === GameStatus.WIN) {
+          store.dispatch<any>({ type: GAME_OVER });
+          store.dispatch<any>({
+            type: GET_GAME_STATS,
+            gameStats: {
+              score: this.score.getValue(),
+              completedTime: this.timer.getValue(),
+            },
+          });
+        }
         this.game.events.off(EVENTS_NAME.keyChest, this.keyChestHandler);
         this.game.events.off(EVENTS_NAME.coinChest, this.coinChestHandler);
         this.game.events.off(
